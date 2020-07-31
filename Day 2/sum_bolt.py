3b0f0bff49da05157a3e4727e231e77473f2c294
Restyled.io <commits@restyled.io>
2020-07-31 08:38:19 +0000

    Restyled by black

--- Day 2/sum_bolt.py
+++ Day 2/sum_bolt.py
@@ -6,11 +6,11 @@ def max_pairwise_product(a):
     for i in range(2):
         x = max(a)
         b.append(a.pop(a.index(x)))
-    sum = b[0]*b[1]
+    sum = b[0] * b[1]
 
     return sum
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
